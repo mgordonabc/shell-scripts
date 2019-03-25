6e90a10ee8d1493d592a355378dc0f1e60ac996a
eservices provider <mgordonabc@gmail.com>
2019-03-25 09:22:38 -0500

    add gradle install commands

---
+++ install_gradle.sh
@@ -0,0 +1,9 @@
+
+nano /etc/apt/sources.list
+deb http://ftp.debian.org/debian/ stable main contrib non-free
+deb http://ftp.de.debian.org/debian jessie main
+
+
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install gradle
